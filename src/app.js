ad306ec4c70be476b17643b793b4b38844be1766
Mary Zam <zamo.code@gmail.com>
2018-01-25 23:51:00 +0300

    Add coeff information nearby of each curve, unify drawing speed

--- src/app.js
+++ src/app.js
@@ -5,21 +5,22 @@ import { getGridSize } from "./utils/get-grid-size"
 const container = d3.select(".container");
 const size = container.node().getBoundingClientRect();
 
-const coeffs = getRoseCoefficients(9, 9);
+const coeffs = getRoseCoefficients(7, 9);
 const grid = getGridSize(coeffs.length, size);
 
 const scaleCoeff = d3.scaleLog().domain([coeffs[0].k, coeffs[coeffs.length - 1].k]);
 const roseColor = d3.scaleSequential((d) => d3.interpolateWarm(scaleCoeff(d)));
-const roseRadius = (grid.cellSize - 10) / 2;
+const roseRadius = (grid.cellSize - 10) / 2 - 3;
 
 const line = d3.lineRadial()
                     .angle((d) => d.angle)
                     .radius((d) => d.radius)
-                    .curve(d3.curveCardinal);
+                    .curve(d3.curveBasis);
 
 function generateRose(coeff) {
     const period = (coeff.p & coeff.q & 1 ? 1 : 2) * Math.PI * coeff.q;
-    const points = d3.range(0, period + Math.PI / 20, Math.PI / 20)
+    const step = period / 100;
+    const points = d3.range(0, (period + step), step)
         .map(function(theta) {
             const r = roseRadius * Math.sin(coeff.k * theta);
             return { radius: r, angle: theta };
@@ -63,32 +64,54 @@ blocks
         .attr("class", "dynamic")
         .style("stroke", (d) => roseColor(d.coeff.k));
 
+const textOffset = -grid.cellSize / 2;
+const textTranslate = `translate(${textOffset},${textOffset + 10})`;
+
+blocks
+    .append("text")
+    .attr("transform", textTranslate)
+    .style("fill-opacity", "0.3")
+    .text((d) => { return `k = ${d.coeff.p}/${d.coeff.q}`; });
+
 blocks
     .append("rect")
-        .attr("x", -roseRadius)
-        .attr("y", -roseRadius)
+        .attr("x", -grid.cellSize / 2)
+        .attr("y", -grid.cellSize / 2)
         .attr("width", grid.cellSize)
         .attr("height", grid.cellSize)
             .on("mouseover", function(d) { startDrawingCurve(d, this); })
             .on("mouseout", function(d) { stopDrawingCurve(d, this); });
 
-
 let timer = null;
 function startDrawingCurve(data, handler) {
-    const dynamicPath = d3
-        .select(handler.parentNode)
+    const parent = d3
+        .select(handler.parentNode);
+
+   // show coeff info label
+    parent
+        .select("text")
+        .style("fill-opacity", 1);
+
+    // start drawing a curve
+    const dynamicPath = parent
         .select("path.dynamic")
         .attr("d", "");
+    if (timer !== null) {
+        clearInterval(timer);
+    }
     let ticks = 0;
     timer = setInterval(function() {
-            dynamicPath
-                .attr("d", (d) => line(d.points.slice(0, ticks)));
+            dynamicPath.attr("d", (d) => line(d.points.slice(0, ticks)));
             ticks++;
-        },
-        50);
+        }, 50);
 }
 
 function stopDrawingCurve(data, handler) {
+    // hide coeff info label
+    d3.select(handler.parentNode)
+      .select("text")
+        .style("fill-opacity", 0.3);
+    // stop drawing a curve
     clearInterval(timer);
     timer = null;
 }
